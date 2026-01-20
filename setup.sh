b50cf3c1dc1706aaecb5f67ff3e3204d7507ffcc
Kylian Schmidt <kylian.schmidt@kit.edu>
2026-01-20 16:31:08 +0100

    Revert "add setup.sh script to link gem libraries"
    This reverts commit 398b5ebf5dd6e20304d2289879b2dea554574b92.

--- setup.sh
+++
@@ -1,9 +0,0 @@
-# !/bin/bash
-NEEDLE_GREEN='\033[0;32m'
-NEEDLE_NC='\033[0m' # No Color
-
-# Install Ruby Gems to ~/gems
-export GEM_HOME="$HOME/gems"
-export PATH="$HOME/gems/bin:$PATH"
-
-echo -e "${NEEDLE_GREEN}Setup successful${NEEDLE_NC}"
